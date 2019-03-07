516da6b376a207bc460cf46a7dabee5091fffbd0
chriskes <chriskesarsing@gmail.com>
2019-03-07 15:15:21 +0100

    Added server functionality, added pugjs

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var lodash = require('lodash');
+var nodemon = require('nodemon');
 var validator = require('validator');
+const express = require('express'); 
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('static'));
+
+app.get('/', function (req, res) {
+    res.render('index.pug')
+  })
+
+app.listen(8000);
