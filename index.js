08f5bf47c4797a85960a954c5e0205cca610c1fd
chriskes <chriskesarsing@gmail.com>
2019-03-08 20:26:20 +0100

    Added login

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
+//--------- REQUIRE PACKAGES ---------//
 const nodemon = require('nodemon');
 const validator = require('validator');
 const express = require('express');
 const path = require('path');
 const app = express();
 
+//--------- INITIALIZE PUG TEMPLATING ENGINE ---------//
 app.set('view engine', 'pug');
 
+//--------- CREATE VIRTUAL PATH TO STATIC DIRECTORY ---------//
 app.use('/static', express.static('static'));
 
+//--------- HOME PAGE ---------//
 app.get('/', function (req, res) {
   res.render('index.pug')
 })
 
+//--------- ABOUT PAGE ---------//
 app.get('/about', function (req, res) {
   res.render('about.pug')
 })
 
+//--------- LOGIN PAGE ---------//
+app.get('/login', function (req, res) {
+  res.render('login.pug')
+})
+
+//--------- 404 ---------//
 app.use(function (req, res, next){
   res.status(404).sendFile(path.join(__dirname, '/static/404.html'));
 })
 
+//--------- PORT ---------//
 app.listen(8000);
