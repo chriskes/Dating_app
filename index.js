79b3e4761a651dc82ede62d2835b84e2e301299e
chriskes <chriskesarsing@gmail.com>
2019-03-08 20:12:52 +0100

    Added about page, routed 404 to static html file. # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Your branch is up to date with 'origin/master'.## Changes to be committed:#	modified:   index.js#	renamed:    static/index.html -> static/404.html#	modified:   static/styles.css#	new file:   views/about.pug#

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var nodemon = require('nodemon');
-var validator = require('validator');
-const express = require('express'); 
+const nodemon = require('nodemon');
+const validator = require('validator');
+const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'pug');
@@ -8,7 +9,15 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('static'));
 
 app.get('/', function (req, res) {
-    res.render('index.pug')
-  })
+  res.render('index.pug')
+})
+
+app.get('/about', function (req, res) {
+  res.render('about.pug')
+})
+
+app.use(function (req, res, next){
+  res.status(404).sendFile(path.join(__dirname, '/static/404.html'));
+})
 
 app.listen(8000);
