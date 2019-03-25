7314100773329f22818bcaf4c224f21378b4763b
chriskes <chriskesarsing@gmail.com>
2019-03-25 22:23:02 +0100

    Data feature

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const express = require('express');
 const find = require('array-find');
-var upload = multer({dest: 'static/upload/'})
+var upload = multer({dest: 'client/upload/'})
 
 var data = [{
-    id: 'evil-dead',
-    title: 'Evil Dead',
-    plot: 'Five friends travel to a cabin in the …',
-    description: 'Five friends head to a remote …'
-  },
-  {
-    id: 'the-shawshank-redemption',
-    title: 'The Shawshank Redemption',
-    plot: 'Two imprisoned men bond over a number …',
-    description: 'Andy Dufresne is a young and  …'
-  }
+  id: 'johan-keizer',
+  name: 'Johan Keizer',
+  age: '25',
+  interests: 'Piet Mondriaan'
+},
+{
+  id: 'alex-zwart',
+  name: 'Alex Zwart',
+  age: '34',
+  interests: 'Pablo Picasso'
+}
 ]
 
 express()
@@ -27,11 +27,11 @@ express()
   .set('view engine', 'ejs')
   .set('views', 'server')
   .get('/', home)
-  .get('/list', movies)
+  .get('/list', accounts)
   .get('/add', form)
-  .get('/:id', movie)
+  .get('/:id', account)
   .delete('/:id', remove)
-  .post('/', upload.single('cover'), add)
+  .post('/', upload.single('profile'), add)
   .use(notFound)
   .listen(8000)
 
@@ -39,39 +39,38 @@ function home(req, res) {
   res.render('home.ejs')
 }
 
-function movies(req, res) {
+function accounts(req, res) {
   res.render('list.ejs', {data: data})
 }
 
-function movie(req, res, next) {
+function account(req, res, next) {
   var id = req.params.id
-  var movie = find(data, function (value) {
+  var detail = find(data, function (value) {
     return value.id === id
   })
 
 //---------- Statement below prevents the callback from triggering twice, preventing error ----------//
-  if (!movie) {
+  if (!account) {
     next();
     return;
   }
 
-  res.render('detail.ejs', {data: movie})
+  res.render('detail.ejs', {data: detail})
 }
-
 
 function form(req, res) {
   res.render('add.ejs')
 }
 
 function add(req, res) {
-  var id = slug(req.body.title).toLowerCase()
+  var id = slug(req.body.name).toLowerCase()
 
   data.push({
     id: id,
-    title: req.body.title,
-    cover: req.file ? req.file.filename : null,
-    plot: req.body.plot,
-    description: req.body.description
+    name: req.body.name,
+    profile: req.file ? req.file.filename : null,
+    age: req.body.age,
+    interests: req.body.interests
   })
 
   res.redirect('/' + id)
